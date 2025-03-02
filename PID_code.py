ba27c62fb926de7cb10e61575d17458499df45f1
MOHANAD 10 <eng.mohanadalexu@gmail.com>
2025-03-02 23:14:08 +0200

    Add files via upload

--- PID_code.py
+++ PID_code.py
@@ -5,6 +5,8 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 import math 
+from std_msgs import Int16
+from functools import partial
 
 
 class PID():
@@ -29,6 +31,15 @@ class PID():
     def Control(self);
         pass
 
+class Encoder(Node):
+    def __init__(self):
+        super().__init__('encoder_node')
+        self.create_subscription(Int16,'/encoder_ticks',10,self.call_back_encoder)
+
+    def call_back_encoder(self,msg:Int16):
+        self.ticks = msg.data
+
+        
 class Trajectory(Node):
     def __init__(self):
         super().__init__('Trajectory')
@@ -37,19 +48,39 @@ class Trajectory(Node):
 
 
 
+
     def odom_call_back(self,msg:Odometry):
-        pass
+       self.robot_x = msg.pose.pose.postion.x
+       self.robot_y = msg.pose.pose.postion.y
+
+       
 
 
-        
-        
+    def cmd_vel_service(self):
+        client = self.create_client(Twist,'cmd_vel')
+        while not client.wait_for_service(1.0):
+            self.get_logger().warn("waiting for service")
 
-        
+        request =Twist.Request()
+        request.linear.x = # liner veolcity from pid 
+
+
+        future = client.call_async(request)
+        future.add_done_callback(partial(self.callback_set_pen))
+
+
+
+    def callback_set_pen(self,future):
+        try:
+             response = future.result()
+        except Exception as e:
+            self.get_logger().error(f"service call failed :{e}")
+
+
+       
 
 def main(args=None):
     rclpy.init(args=args)
-     
-
     node = Trajectory()
     rclpy.spin()
     rclpy.shutdown()
