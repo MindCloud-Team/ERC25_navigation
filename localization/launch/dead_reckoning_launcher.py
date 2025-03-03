83eeb82a63358ccdf13c173494d3f97732b1c883
Mwa195 <mohamedwael200320@gmail.com>
2025-03-03 19:22:44 +0200

    ADD: Launch File

---
+++ localization/launch/dead_reckoning_launcher.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='localization',
+            executable='dead_reckoning',
+            name='localization_imu_encoder',
+            output='screen',
+            parameters=[{
+                'rate': 50,
+                'wheel_diameter': 0.12,
+                'ticks_per_rev': 600,
+                'frame_id': 'odom',
+                'child_frame_id': 'base_link'
+                }
+            ]
+        )
+    ])
